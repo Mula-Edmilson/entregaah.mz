06c3acf121c123feb9a1ed2784e229c30e7a929f
Mula-Edmilson <mula.edmilson0@gmail.com>
2026-01-14 20:25:51 +0200

    Add files via upload

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const { body } = require('express-validator');
 const authController = require('../controllers/authController');
-const { protect, admin } = require('../middleware/authMiddleware');
+const { protect, admin, driver  } = require('../middleware/authMiddleware');
 const { validateRequest } = require('../middleware/validateRequest');
 
 const router = express.Router();
 
 const { getMe } = require('../controllers/authController');
-const { protect } = require('../middleware/authMiddleware');
 
 router.get('/me', protect, getMe);
 
